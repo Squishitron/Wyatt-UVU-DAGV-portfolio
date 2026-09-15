d9c578ca46415b193732a40a42e3591c319f2d7e
Squishitron <Squishitron@gmail.com>
2026-09-14 21:49:33 -0600

    Chore: fixing drag
    so yeah that took a lot of unexpected work, and I didnt get to interactions yet, but its fixed now

--- Unity Projects/Reload Stall/Assets/scripts/draggable object.cs
+++ Unity Projects/Reload Stall/Assets/scripts/draggable object.cs
@@ -1,21 +1,90 @@
+using System;
+using UnityEditor.UI;
 using UnityEngine;
 
 public class draggableobject : MonoBehaviour
 
 {
 
+    //setting up to find screen and world positions
     Vector3 mousePos;
     RaycastHit2D raycastHit2D;
     Transform clickObject;
     //public to be set in inspector
     public Camera mainCamera;
+    //setting variable to track if this object is grabbable or not, public idealy with default false
+    //if that public could be a dropdown menu would be super ideal
+    public Boolean isGrabbable = false;
+    //referencing the gripper object so we can access its script to check against its hover variable
+    //damn, not allowed to use find
+    //so thisVVVV is returning null, maybe if we just directly find the object and its variable and just store the variable we want right away?
+
+    //private GameObject grippers = GameObject.Find("GRIPPAH");
+
+    //so apparently .Find has to be called in start or update and I've been running in circles for no fucking reason and now Im disgruntled
+    //new plan, declare private gameobject here, leave it empty, in start find the other fucking object (which actually makes sense now Im thinking about it)
+    //because none of the stuff we're sniffing for actually happens until AFTER the game starts running, so makes sense in hindsight
+    //anyway, we then check against it to see if grabby stuff happens
     
+    //ok its defined, now fill it during start VVV
+    GameObject grippers;
+
+
+
+
+    //so anyway this was all garbo in the end VVV
+    //it pointed me towards trying some "object sterilization or something" we'll try that
+    //ok I think I found the syntax, its serialize / serialized
+    //[SerializeField] private sticktomouse stickymouse;
+    //ok, in theory this should allow us to use variables from the stick to mouse script
+    //it might have except we need the object specific version and .find just works actually like actually cause lying internet ai
+
+
+
+
+    //testing a random variable to see if we can pull it from stick to mouse (we can absolutely just via variable name, and also just object name)
+    /*that means we can freely use a system like this to check if the object we are hovering is interactable
+     or not, which in theory is to be local, but keep the variable public so that we can set it per object via the inspector so that we can use this
+    same script for all hoverable objects regardless of if we want them to be grabbable or not
+    maybe this should also be changable by other collisions so that we can turn off the grabbability of object as they are used in the game world
+    so maybe a getter and setter might be appropriate afterall, or at least a setter for when bullets will eventually collide with the magazine*/
+    
+    //so it worked
+    //public string sticktoMousetest = "default";
+
 
 
     //is this yitch grabbed rn? cause clickObject doesnt constantly update
     bool isGrabbed = false;
 
+    // not sure what mouseDown does cause I forgot to document when I wrote it, and Im afraid to delete it
     bool mouseDown = false;
+
+
+
+    /*
+    string GetMouseTest()
+    {
+
+        return sticktoMousetest;
+
+    }
+    so turns out we dont actually need a get command because protection level BS but also because we can
+    just directly call for the variable from stick to mouse directly, so we should be good to go as long as we can
+    check name and tag the same way for parent object of this script to check against the mouse version
+    to allow or dissalow the dragging function so it will not fire to everything all at once like it did before */
+
+
+    void Start()
+    {
+
+        //this should hopefully actually work when done down here, and actually fill our temp variable with the proper gameobject
+        // YES YES YES YES YES YES YES YES YES YES YES IT FINALLY FUCKING WORKED
+        //just had to think and research for a tad, but it makes sense why in the end, even if it burned almost 2 hours
+        grippers = GameObject.Find("GRIPPAH");
+
+    }
+
 
     void Update()
     {
@@ -28,45 +97,51 @@ public class draggableobject : MonoBehaviour
         Vector3 mouseWorld = mainCamera.ScreenToWorldPoint(mousePos);
 
         //raycast from screen space to check for clickable objects
-        Ray mouseRay = Camera.main.ScreenPointToRay(mousePos);
+        //Ray mouseRay = Camera.main.ScreenPointToRay(mousePos);
 
         //when clicking
         if (Input.GetMouseButtonDown(0))
         {
-            //checking if cursor is over clickable object
-            raycastHit2D = Physics2D.Raycast(mouseRay.origin, mouseRay.direction);
-            clickObject = raycastHit2D ? raycastHit2D.collider.transform : null;
 
-            //if clickable object clicked
-            if (clickObject)
+            //this should check to see if the parent object is the one the mouse is actually hovering over
+            //we have to use stickymouse instead of sticktomouse because we have to use our serialized name instead of the actual
+            //class name
+
+            //FINALLY YES YES YES YES YES YES YES YES WE CAN GRAB INDIVIDUAL OBJECTS NOW!!!!
+            if (this.gameObject.name == grippers.GetComponent<sticktomouse>().hoveredOver)
             {
-                //debug change color to red
-                clickObject.GetComponent<SpriteRenderer>().color = Color.red;
 
-                //set yitch grabage to true
                 isGrabbed = true;
 
-
-            }
+            }    
 
         }
+        //testing to see if this ^ is working
+        Debug.Log(isGrabbed);
+        //so apparently .find does just fucking work afterall, and google AI is a lying son of a bitch like always
+        //so Imma go back and remove the serialization BS
+        //Debug.Log(GameObject.Find("GRIPPAH"));
+        //lets see if this works
+        //hrmm, grippers appears to be returning NULL
+        //that would explain why it didnt work
+        Debug.Log(grippers.GetComponent<sticktomouse>().hoveredOver);
+        //so now its filling with a game object, but it wont fucking let me do gameobject things and its saying something about "method groups"
+        //so turns out I was just missing a pair of parentheses AFTER the fucking triangle ones for some reason even tho I did it before and should
+        //have known better and now Im kinda mad at myself
+        //congrats at costing almost 2 hours parentheses
+        //it works now, Imma leave it uncommented for now so I can remember it for a little while
+
+
 
         //when letting go
+        //letting go should be globally applicable, regardless of if its being hovered or not
         if (Input.GetMouseButtonUp(0))
         {
-            //if letting go of clickable object
-            if (clickObject)
-            {
-                //debug return to defualt color
-                clickObject.GetComponent<SpriteRenderer>().color = Color.white;
 
-                //set yitch grabbed to false when letting go
-                isGrabbed = false;
-
-            }
-
+            isGrabbed = false;
 
         }
+
 
         //if this yitch is grabbed, move the object to mouse world position
         if (isGrabbed == true)
@@ -75,6 +150,24 @@ public class draggableobject : MonoBehaviour
             transform.position = mouseWorld;
 
         }
+
+
+
+
+        /*ok so its testing time
+        lets see if we can find whatever parrent object this script is attached to's name and print it to console
+        there should be a tad bit of noise in the console because multiple objects already have this script attached
+        hopefully in the long run this wont cause too much of a performance impact, but we if we needed to out of desperation
+        we could just in theory bring back a copy of the mouse ray script so that we could just enable dissable this on the fly as needed
+        depending on whether or not this thing is getting hovered over by the mouse*/
+
+
+        //there is absolutley no didly freaking way its as simple as just fumfing "THIS"
+        //whatever I guess this works, so we can make a quick block to check if whatever is being hovered by the mouse just matches our own name to drag
+        //brb gonna draw a quick stupid ball so I can just mass produce grabbable objects real quick for testing
+        
+        //Debug.Log(this.gameObject.name);
+
 
     }
 
--- Unity Projects/Reload Stall/Assets/scripts/stick to mouse.cs
+++ Unity Projects/Reload Stall/Assets/scripts/stick to mouse.cs
@@ -1,26 +1,45 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class sticktomouse : MonoBehaviour
 {
+    //not actually sure what these do
+    //I really fucking wish I knew what this did but I dont have time to research that rabbit hole rn
+    //just fuffing trust the docs for a minute me
+    RaycastHit2D raycastHit2D;
+    Transform clickObject;
 
+    //for use later to find world position and stick this things position to the mouse
     Vector3 myPos;
     Vector3 mousePos;
+
+    //setting for main camera in inspector
     public Camera mainCamera;
     //public sprites to set in inspector
     public Sprite spriteOpen;
     public Sprite spriteClosed;
+
+    //finding sprite renderer for scripts use
     private SpriteRenderer spriteRenderer;
+
+    //setting up for the mouse hover, so we can use it to communicate whats being hovered over
+    //nothing by default, to change with hovering
+    //public so it can be referenced in draggable, but hidden so no appears in inspector because I dont want manual control of this one
+    [HideInInspector]
+    public String hoveredOver = "nothing";
+
     
+    //called at start
     void Start()
     {
-
+        //setting spriterender to this objects component for easier use later
         spriteRenderer = GetComponent<SpriteRenderer>();
 
     }
 
 
-    // Update is called once per frame
+    // update per frame
     void Update()
     {
 
@@ -33,19 +52,91 @@ public class sticktomouse : MonoBehaviour
         Vector3 worldPos = mainCamera.ScreenToWorldPoint(mousePos);
 
 
+
+        //setting ray pos and direciton to match mouse.... I think (not totally sure)
+        Ray mouseRay = mainCamera.ScreenPointToRay(mousePos);
+
+        //using our mouseRay to actually shoot a ray and check for object collisions with a 2D collider
+        raycastHit2D = Physics2D.Raycast(mouseRay.origin, mouseRay.direction);
+        clickObject = raycastHit2D ? raycastHit2D.collider.transform : null;
+        
+        //ok, time for the real hovering ID block
+        //so we got the other side working, but now we can teleport objects, so we need to make it so the hover ID resets when you're not
+        //actively hovering over something
+        if (clickObject)
+        {
+
+            hoveredOver = raycastHit2D.collider.name;
+            //Debug.Log(hoveredOver);
+
+        }
+        else
+        {
+
+            hoveredOver = "NOTHING!!!! HAHAHAHAHHAHA";
+
+        }
+
+
+
+        /*
+
+        Im gonna keep this here for now for posterities sake, so I can tell what was happening and how to continue
+
+        Ill make a new version of this block for the actual use case
+
+        //diagnostic for now
+        //checking to see if the ray is properly hitting 2D colliders, and if so printing to log that it is functioning
+        //good news it works, now lets try testing if we can infact pull other objects components when getting hit with the ray
+        //otherwise Ill have to do it the stupid way
+        if (clickObject) {
+
+            Debug.Log("this shizzz worky");
+            Debug.Log(raycastHit2D.collider);
+            //this works and does actually return a string
+            //now we save this as a variable string to check against names to tell what mouse is hovering over
+            Debug.Log(raycastHit2D.collider.name + " pp poo poo");
+            
+            //turns out we can just read varaibles from draggableobject by just calling the variable name of the component like this
+            //so thats nice and easy
+            Debug.Log(raycastHit2D.collider.GetComponent<draggableobject>().sticktoMousetest);
+
+        } else { 
+        
+
+            Debug.Log("there freaking nothing boss");
+            
+
+        }
+
+        */
+
+
+
+
+
         //myPos = mousePos;
         //unused
 
+
+        //note to me, maybe dont split these functionalities apart from one another with the raycast, maybe fix that later
         //updating myPos to match worldPos of the mouse cursor
         myPos = worldPos;
 
-        //moves object to new myPos
+        //moves the scripts attached object to new myPos
         transform.position = myPos;
 
 
+
+        //diagnostic BS
         //Debug.Log(mousePos);
         //Debug.Log(myPos);
-        Debug.Log(worldPos);
+        //Debug.Log(worldPos);
+
+
+        //these VVVV down there will likely change in the future when mouse context is added so that different objects will have different sprites
+        //when open or closed depending on what grab animation those specific objects should use
+        //for now tho these will work, because this prototype needs to get done before you worry about polish you dingdong
 
         //when clicking close the hand
         if (Input.GetMouseButtonDown(0))
@@ -55,6 +146,7 @@ public class sticktomouse : MonoBehaviour
 
         }
 
+        //when unclicking open the hand
         if (Input.GetMouseButtonUp(0))
         {
 
